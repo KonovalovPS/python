a631dd97b39d7b3eeb941c838cf3bae97902fbf1
Pavel Konovalov <konovalov.ps@mail.ru>
2018-10-16 21:04:56 +0300

    some updates

--- homeworks/log_parse/is_true.py
+++ homeworks/log_parse/is_true.py
@@ -1,16 +1,16 @@
 def is_true(line):
     
-    pattern = '[*/*/*] "* *" *'
+    pattern = '[*/*/*] "* *://*.*" * *'
     def search(line, pattern, k):                                
 
-        for i in range(len(line) - len(pattern) + 1):                   
+        for pat_position in range(len(line) - len(pattern) + 1):                   
             counter = 0 
-            for j in range(len(pattern)):
-                if pattern[j] == line[i + j]:
+            for each in range(len(pattern)):
+                if pattern[each] == line[pat_position + each]:
                     counter += 1
                     
             if counter == len(pattern):
-                k = i + j
+                k = pat_position + each
                 return {'result': True, 'k': k}
 
         return {'result': False, 'k': k}
@@ -19,10 +19,10 @@ def is_true(line):
     def search_stars(line, pattern):                           
         k = 0
         arr = pattern.split('*')
-        for i in range(len(arr)):
-            if arr[i] == '':
+        for index in range(len(arr)):
+            if arr[index] == '':
                 continue
-            if not search(line[k:], arr[i], k)['result']:            
+            if not search(line[k:], arr[index], k)['result']:            
                 return False      
         return True
     
--- homeworks/log_parse/log_parse.py
+++ homeworks/log_parse/log_parse.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-import collections
+from collections import defaultdict, Counter
 from dateutil.parser import parse
 import is_true      #функция из первого ДЗ, чтобы отсеять строки не являющиеся записями об обращении к серверу
 import datetime
@@ -18,16 +18,15 @@ def get_type(s):
     start = s.index('"')
     return s[start + 1 : s.index(' ', start)]
     
-def del_www(s):
-    idx = s.index('://')
-    if 'www' in s[idx + 3: idx + 6]:
-        s = '{}{}'.format(s[ :idx + 3], s[idx + 7:])
-    return s
+def del_www(url):
+    if url[:4] == 'www.':
+        url = url[4:]
+    return url
     
-def response_time(s):
+def get_response_time(s):
     return int(s.split()[-1])
     
-def is_file(s):
+def is_it_a_file(s):
     if get_url(s)[-1] == '/':
         return False
     return True
@@ -42,30 +41,37 @@ def parser(
     slow_queries=False
 ):
 
-    output_arr = [] 
-    arr = []  #массив урлов, удовлетворяющих условиям
-    most_slowly = collections.defaultdict(lambda: [0, 0])
+    output_arr = []
+    most_slowly = defaultdict(lambda: [0, 0])
     f = open('log.log')
+    counter = Counter()
     for line in f:
-        if is_true.is_true(line):
-            if (start_at == None or get_time(line) >= start_at) and\
-            (stop_at == None or get_time(line) <= stop_at) and\
-            get_url(line) not in ignore_urls and\
-            (get_type(line) in ignore_urls or ignore_urls == []) and\
-            (ignore_files == False or is_file(line) == False):
-                if ignore_www == True:
-                    line = del_www(line)
-                arr.append(get_url(line))
-                most_slowly[get_url(line)][0] += 1
-                most_slowly[get_url(line)][1] += response_time(line)   
+        if is_true.is_true(line): 
+            time = get_time(line)
+            type = get_type(line)
+            url = get_url(line)   
+            response_time = get_response_time(line)
+            is_file = is_it_a_file(line)
+            if ignore_www:
+                url = del_www(url)
+            
+            if (start_at == None or time >= start_at) and\
+            (stop_at == None or time <= stop_at) and\
+            url not in ignore_urls and\
+            (type == request_type or request_type == None) and\
+            (ignore_files == False or is_file == False):
+                counter[url] += 1
+                most_slowly[url][0] += 1
+                most_slowly[url][1] += response_time
+                
     if slow_queries == True:
         for each in most_slowly:
             output_arr.append(most_slowly[each][1] // most_slowly[each][0])
-        output_arr = sorted(output_arr, reverse = True)    
-    else:
-        counter = collections.Counter(arr)
+        output_arr = sorted(output_arr, reverse = True)[:5] 
+    else:    
         for elem, count in counter.most_common(5):
-            output_arr.append(count)  
-    print(output_arr[:5])    
+            output_arr.append(count)   
+            
+    print(output_arr)
 
 parser()
