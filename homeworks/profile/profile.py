8ae419459c3b8fce60066a8c8030764b88dc12aa
Pavel Konovalov <konovalov.ps@mail.ru>
2018-11-17 23:02:39 +0300

    some corrections

--- homeworks/profile/profile.py
+++ homeworks/profile/profile.py
@@ -1,28 +1,27 @@
 import datetime
 from inspect import isfunction, isclass
 
+def func_update(func, s=''):
+    def new_func(*args, **kwargs):
+        print('`{}{}` started'.format(s, func.__name__))
+        time1 = datetime.datetime.now()
+        a = func(*args, **kwargs)
+        time2 = datetime.datetime.now()
+        time_func = (time2 - time1).total_seconds()
+        print('`{}{}` finished in {}s'.format(s, func.__name__, time_func))
+        return a
+    return new_func
+
 def profile(obj):
     if isfunction(obj):
-        def new_func(*args, **kwargs):
-            func = obj
-            print('`{}` started'.format(func.__name__))
-            time1 = datetime.datetime.now()
-            a = func(*args, **kwargs)
-            time2 = datetime.datetime.now()
-            time_func = (time2 - time1).total_seconds()
-            print('`{}` finished in {}s'.format(func.__name__, time_func))
-            return a
-        return new_func
-
-
+        return func_update(obj)
     if isclass(obj):
         klass = obj
         for attr_name in klass.__dict__:
             attr = getattr(klass, attr_name)
-            if attr_name != '__dict__':
-                setattr(klass, attr_name, profile(attr))
+            if attr_name != '__dict__' and callable(attr):
+                setattr(klass, attr_name, func_update(attr, s='{}_obj.'.format(klass.__name__)))
         return klass
-
 
 @profile
 def stupid_func():
@@ -32,13 +31,13 @@ def stupid_func():
     print("I'm vegan")
 
 @profile
-class A:
+class Myclass:
     def __init__(self, a):
         self.a = a
 
     def palma(self):
         print('I love palmas')
-
+        
 stupid_func()
-a = A(1)
+a = Myclass(1)
 a.palma()
